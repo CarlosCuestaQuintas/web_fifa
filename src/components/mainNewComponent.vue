f979d01d0e070f683b9d1a4ee4784c9544e591ce
Carlos <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2024-10-17 20:19:37 +0200

    Arreglado el texto del contenedor izquierdo

--- src/components/mainNewComponent.vue
+++ src/components/mainNewComponent.vue
@@ -11,13 +11,14 @@
             </div>
         </div>
         <div class="right-container">
-            <Splide id="News-Splide" :options="{rewind:true,width:'100%',height:'calc(90vh - 94px)'}" aria-label="Erebus">
+            <!-- <Splide id="News-Splide" :options="{rewind:true,width:'100%',height:'calc(90vh - 94px)'}" aria-label="splide-principal"> -->
+            <Splide id="News-Splide" :options="{rewind:true,width:'100%',height:'calc(90vh - 94px)'}" aria-label="splide-principal">
                 <SplideSlide>
-                    <img class="splide__slide" src="https://digitalhub.fifa.com/transform/afd4f74d-76d1-4800-b347-3b2f5300bc38/Lionel-Messi-hat-trick-vs-Bolivia?&io=transform:fill,aspectratio:16x9,width:750&quality=75" alt="Messi">
+                    <img class="splide__slide" src="https://digitalhub.fifa.com/transform/afd4f74d-76d1-4800-b347-3b2f5300bc38/Lionel-Messi-hat-trick-vs-Bolivia?&io=transform:fill,aspectratio:16x9,width:750&quality=100" alt="Messi">
                 </SplideSlide>
                 <!-- https://i.redd.it/2adecof06t651.jpg -->
                 <SplideSlide>
-                    <img class="splide__slide" src="https://digitalhub.fifa.com/transform/492ef829-93a3-4c80-b972-d5dad83c3ac8/2178117227?focuspoint=0.51,0.38&io=transform:fill,aspectratio:16x9&quality=75" alt="Peperebus 2">
+                    <img class="splide__slide" src="https://digitalhub.fifa.com/transform/492ef829-93a3-4c80-b972-d5dad83c3ac8/2178117227?focuspoint=0.51,0.38&io=transform:fill,aspectratio:16x9&quality=75" alt="Dominicanas">
                 </SplideSlide>
             </Splide>
 
