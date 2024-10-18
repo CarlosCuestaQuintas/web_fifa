c96007fbd245056869494e5d44b84767fc885873
Carlos <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2024-10-18 13:23:54 +0200

    Añadida la clase texto-azul en styles.scss y cambiadas las ids por clases en directoComponent.vue

--- src/components/directoComponent.vue
+++ src/components/directoComponent.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="directo-container">
-        <div id="superior-directo">
-            <div id="titulo-directo" class="texto-azul">MIRA EN VIVO EN FIFA+</div>
-            <div id="mostrar-todo-directo"><a href="#">MOSTRAR TODO</a></div>
+    <div class="directo-container">
+        <div class="superior-directo">
+            <div class="titulo-directo texto-azul">MIRA EN VIVO EN FIFA+</div>
+            <div class="mostrar-todo-directo"><a href="#">MOSTRAR TODO</a></div>
         </div>
     </div>
 </template>
